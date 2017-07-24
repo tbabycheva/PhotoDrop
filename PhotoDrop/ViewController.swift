c49eda54bebf04144404b073c5eaa10681a66f12
tbabycheva <tetianababycheva@gmail.co>
2017-07-24 14:38:18 -0600

    Initial Commit

---
+++ PhotoDrop/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PhotoDrop
+//
+//  Created by Tetiana Babycheva on 7/24/17.
+//  Copyright © 2017 babycheva. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
