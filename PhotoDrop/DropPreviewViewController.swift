e49d013d29fd7a97a0244b89cc124461f526594c
Kennefofearf <kpeterson344@gmail.com>
2017-07-25 15:01:28 -0600

    Added a drop preview storyboard and view controller.

---
+++ PhotoDrop/DropPreviewViewController.swift
@@ -0,0 +1,19 @@
+//
+//  DropPreviewViewController.swift
+//  PhotoDrop
+//
+//  Created by Kenny Peterson on 7/25/17.
+//  Copyright © 2017 babycheva. All rights reserved.
+//
+
+import UIKit
+
+class DropPreviewViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+
+}
--- PhotoDrop/DropViewController.swift
+++ PhotoDrop/DropViewController.swift
@@ -13,7 +13,8 @@ class DropViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        
+        
     }
     
     @IBAction func backButtonTapped(_ sender: Any) {
@@ -21,21 +22,5 @@ class DropViewController: UIViewController {
         dismiss(animated: true, completion: nil)
         
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
