1a90b4afd75ffd3584ee8e3a1f1afc32a8ae1be2
tbabycheva <tetianababycheva@gmail.co>
2017-07-28 10:02:11 -0600

    Add icons to camera buttons, constrain them

--- PhotoDrop/DropViewController.swift
+++ PhotoDrop/DropViewController.swift
@@ -11,6 +11,8 @@ import AVFoundation
 
 class DropViewController: UIViewController, AVCapturePhotoCaptureDelegate, UIImagePickerControllerDelegate {
     
+    // MARK: - Properties and Outlets
+    
     var cameraOutput: AVCapturePhotoOutput!
     var cameraSession: AVCaptureSession!
     var camPreviewLayer: AVCaptureVideoPreviewLayer!
@@ -27,17 +29,17 @@ class DropViewController: UIViewController, AVCapturePhotoCaptureDelegate, UIIma
     @IBOutlet weak var flashToggle: UIButton!
     @IBOutlet weak var cameraPositionToggle: UIButton!
 
+    
+     // MARK: - Lifecycle
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         cameraLoad()
-        
     }
     
     func cameraLoad() {
-        
-        view.addSubview(backButton)
-        view.addSubview(takePhotoButton)
+
         imageView.isHidden = true
         
         cameraSession = AVCaptureSession()
@@ -65,18 +67,10 @@ class DropViewController: UIViewController, AVCapturePhotoCaptureDelegate, UIIma
         } else {
             print("An error occurred.")
         }
-        
     }
     
-    @IBAction func flashToggleButtonPressed(_ sender: Any) {
-        
-    }
     
-    @IBAction func cameraPositionTogglePressed(_ sender: Any) {
-        toggleCamera()
-    }
-    
-    // Taking the picture
+    // MARK: - Action Functions
     
     @IBAction func takePhotoButtonTapped(_ sender: Any) {
         
@@ -92,48 +86,45 @@ class DropViewController: UIViewController, AVCapturePhotoCaptureDelegate, UIIma
         cameraOutput.capturePhoto(with: settings, delegate: self)
     }
     
+    @IBAction func backButtonTapped(_ sender: Any) {
+        
+        dismiss(animated: true, completion: nil)
+    }
+    
+    @IBAction func flashToggleButtonPressed(_ sender: Any) {
+        
+    }
+    
+    @IBAction func cameraPositionTogglePressed(_ sender: Any) {
+        toggleCamera()
+    }
+    
+    
+    // MARK: - Formatting and Saving a Picture
+
     func capture(_ captureOutput: AVCapturePhotoOutput, didFinishProcessingPhotoSampleBuffer photoSampleBuffer: CMSampleBuffer?, previewPhotoSampleBuffer: CMSampleBuffer?, resolvedSettings: AVCaptureResolvedPhotoSettings, bracketSettings: AVCaptureBracketedStillImageSettings?, error: Error?) {
         
         if let error = error {
             print("An error has occured: \(error.localizedDescription)")
         }
-     if let sampleBuffer = photoSampleBuffer,
-        let previewBuffer = previewPhotoSampleBuffer,
-        let dataImage = AVCapturePhotoOutput.jpegPhotoDataRepresentation(forJPEGSampleBuffer: sampleBuffer, previewPhotoSampleBuffer: previewBuffer) {
+        if let sampleBuffer = photoSampleBuffer,
+            let previewBuffer = previewPhotoSampleBuffer,
+            let dataImage = AVCapturePhotoOutput.jpegPhotoDataRepresentation(forJPEGSampleBuffer: sampleBuffer, previewPhotoSampleBuffer: previewBuffer) {
             print(UIImage(data: dataImage)?.size as Any)
-        
-        guard let dataProvider = CGDataProvider(data: dataImage as CFData),
-              let cgImageRef = CGImage(jpegDataProviderSource: dataProvider, decode: nil, shouldInterpolate: true, intent: .defaultIntent)
-            else { return }
-        let image = UIImage(cgImage: cgImageRef, scale: 1.0, orientation: UIImageOrientation.right)
-        
-        self.imageView.image = image
-        
-        performSegue(withIdentifier: "toDropPreview", sender: nil)
+            
+            guard let dataProvider = CGDataProvider(data: dataImage as CFData),
+                let cgImageRef = CGImage(jpegDataProviderSource: dataProvider, decode: nil, shouldInterpolate: true, intent: .defaultIntent)
+                else { return }
+            let image = UIImage(cgImage: cgImageRef, scale: 1.0, orientation: UIImageOrientation.right)
+            
+            self.imageView.image = image
+            
+            performSegue(withIdentifier: "toDropPreview", sender: nil)
         }
     }
+
     
-    func toggleFlash() {
-        
-        
-        
-    }
-    
-    func toggleCamera() {
-        
-        if cameraPosition == AVCaptureDevicePosition.front {
-            cameraPosition = AVCaptureDevicePosition.back
-        } else {
-            cameraPosition = AVCaptureDevicePosition.back
-        }
-        cameraLoad()
-    }
-    
-    @IBAction func backButtonTapped(_ sender: Any) {
-        
-        dismiss(animated: true, completion: nil)
-        
-    }
+    // MARK: - Navigation
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         
@@ -142,7 +133,5 @@ class DropViewController: UIViewController, AVCapturePhotoCaptureDelegate, UIIma
             let dropPreviewVC = segue.destination as? DropPreviewViewController
             dropPreviewVC?.image = image
         }
-        
     }
-
 }
