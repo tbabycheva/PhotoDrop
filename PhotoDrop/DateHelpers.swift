206dbbbe887fe36a1597f8fbd6e632a4a0dc6e68
tbabycheva <tetianababycheva@gmail.co>
2017-07-28 16:20:21 -0600

    Add 'Photo List VC' data source functions

---
+++ PhotoDrop/DateHelpers.swift
@@ -0,0 +1,9 @@
+//
+//  DateHelpers.swift
+//  PhotoDrop
+//
+//  Created by Tetiana Babycheva on 7/28/17.
+//  Copyright © 2017 babycheva. All rights reserved.
+//
+
+import Foundation
--- PhotoDrop/PhotoListViewController.swift
+++ PhotoDrop/PhotoListViewController.swift
@@ -7,56 +7,81 @@
 //
 
 import UIKit
+import MapKit
 
 class PhotoListViewController: UIViewController {
     
+    // Mock data
+    struct Photo {
+        let title: String
+        let username: String
+        let dateString: String
+        let pointsString: String
+    }
+    
+    let photos = [
+        Photo(title: "New Cool Graffiti", username: "robhand23", dateString: "07/24/2017", pointsString: "15pts"),
+        Photo(title: "Awesome Mountain View", username: "tanya_mila", dateString: "09/24/2016", pointsString: "250pts"),
+        Photo(title: "My First Photo", username: "kenny_man_here", dateString: "07/28/2017", pointsString: "10pts")
+    ]
+    
 @IBOutlet weak var photoListTableView: UITableView!
+    
+    
+    // MARK: - Lifecycle
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
     }
     
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        photoListTableView.reloadData()
+    }
+    
+    // MARK: - Action Functions
+    
     @IBAction func mapButtonTapped(_ sender: Any) {
-        
         dismiss(animated: true, completion: nil)
-        
     }
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
 
-extension PhotoListViewController: UITableViewDelegate {
-}
+// MARK: - TableView Data Source
 
 extension PhotoListViewController: UITableViewDataSource {
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         
-        return 1
-        
+        return photos.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "photoCell", for: indexPath) as? PhotoTableViewCell else { return UITableViewCell() }
         
-        let cell = tableView.dequeueReusableCell(withIdentifier: "photoCell", for: indexPath)
+        let photo = photos[indexPath.row]
+        
+        cell.titleLabel.text = photo.title
+        cell.usernameLabel.text = photo.username
+        cell.dateLabel.text = photo.dateString
+        cell.pointsLabel.text = photo.pointsString
+        cell.gemImageView.image = UIImage(named: "diamond-gold")
         
         return cell
-        
     }
+}
+
+
+// MARK: - TableView Delegate
+
+extension PhotoListViewController: UITableViewDelegate {
     
 }
 
+// MARK: - Navigation
+
+extension PhotoListViewController {
+    
+}
 
 
 
