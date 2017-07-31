40bf262d41d5da0a90f22520df73f7d013b05b9a
tbabycheva <tetianababycheva@gmail.co>
2017-07-31 11:27:29 -0600

    Add CloudKit data to PhotoList

--- PhotoDrop/PhotoViewController.swift
+++ PhotoDrop/PhotoViewController.swift
@@ -9,35 +9,15 @@
 import UIKit
 
 class PhotoViewController: UIViewController {
-
+    
     var drop: Drop?
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
     }
     
     @IBAction func backButtonTapped(_ sender: Any) {
-        
         dismiss(animated: true, completion: nil)
-        
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
